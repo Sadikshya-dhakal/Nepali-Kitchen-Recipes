c5e2be8ceb83073d572ad341e2fc0539d05c2df9
Sadikshya Dhakal <dhakalsadikshya52@gmail.com>
2025-12-21 13:23:10 +0545

    creating categories views

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,4 +4,7 @@ from recipes import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("categories/", views.AllCategoriesView.as_view(), name="all-categories"),
+    path("category/<int:pk>/", views.CategoryDetailView.as_view(), name="category-detail"),
+    path("recipe/<int:pk>/", views.RecipeDetailView.as_view(), name="recipe-detail"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from .models import Category, Recipe
-
-# Create your views here.
+from django.views.generic import TemplateView, ListView, DetailView
+from django.db.models import Q
+from .models import Recipe, Category
 
 
 class HomeView(TemplateView):
@@ -11,14 +10,75 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        # Get 6 categories for "Explore Nepal's Culinary Heritage" section
-        context["categories"] = Category.objects.all()[:6]
+        # Get all categories
+        context["categories"] = Category.objects.all()
         
-        # Get 4 trending recipes for "Trending Now" section
+        # Get trending recipes
         context["trending_recipes"] = Recipe.objects.filter(
             published_at__isnull=False, 
             status="active", 
             is_trending=True
         ).order_by("-published_at")[:6]
+        
+        return context
+
+
+class AllCategoriesView(ListView):
+    model = Category
+    template_name = "recipes/category/all_categories.html"
+    context_object_name = "categories"
+    
+    def get_queryset(self):
+        return Category.objects.all().order_by('order', 'name')
+
+
+class CategoryDetailView(ListView):
+    model = Recipe
+    template_name = "recipes/category/category.html"
+    context_object_name = "recipes"
+    paginate_by = 5
+    
+    def get_queryset(self):
+        return Recipe.objects.filter(
+            category__pk=self.kwargs.get('pk'),
+            published_at__isnull=False,
+            status="active"
+        ).order_by("-published_at")
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = Category.objects.get(pk=self.kwargs.get('pk'))
+        return context
+
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = "recipes/detail.html"
+    context_object_name = "recipe"
+    
+    def get_queryset(self):
+        return Recipe.objects.filter(
+            published_at__isnull=False, 
+            status="active"
+        )
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Increment views
+        current_recipe = self.object
+        current_recipe.views_count += 1
+        current_recipe.save()
+        
+        # Related recipes
+        context["related_recipes"] = (
+            Recipe.objects.filter(
+                published_at__isnull=False,
+                status="active",
+                category=self.object.category,
+            )
+            .exclude(id=self.object.id)
+            .order_by("-published_at")[:3]
+        )
         
         return context
